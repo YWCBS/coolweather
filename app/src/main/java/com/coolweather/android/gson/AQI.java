177f017fc1e8917444b3100a450612a3dcf62e09
BYRANT <BYRANT@Laker.com>
2018-12-05 11:41:28 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by mcz6537 on 2018/12/5.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
